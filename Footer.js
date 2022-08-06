f291fee6c8ca8139f1110b1d3bde1d86dcf8f7d4
Nicholas M Goldstein <98927571+nicholasmgoldstein@users.noreply.github.com>
2022-08-06 14:51:07 -0400

    Update Footer.js

--- Footer.js
+++ Footer.js
@@ -7,12 +7,10 @@ function Footer() {
             <label>
                 Join the Newsletter:  
             </label>
-                <form action="mailto:nicholasmgoldstein@gmail.com" method="post" enctype="text/plain">
+                <form action="mailto:--your email--" method="post" enctype="text/plain">
                     <input type="email" placeholder="email@example.com"/>
                     <input type="hidden" value="Please add me to the mailing list!" />
-                    <input type="submit" value="Join">
-
-                    </input>
+                    <input type="submit" value="Join" />
                 </form>
     </footer>
   );
