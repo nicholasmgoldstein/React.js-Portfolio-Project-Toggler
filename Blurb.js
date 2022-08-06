f6d5e2628936767eeebf37f1fe0d59c6d003e39c
Nicholas M Goldstein <98927571+nicholasmgoldstein@users.noreply.github.com>
2022-08-06 14:52:47 -0400

    Update Blurb.js

--- Blurb.js
+++ Blurb.js
@@ -6,16 +6,18 @@ function Blurb() {
     <section id="blurb">
         <div className="blurbBox">
             <p className="blurb"><i>
-            Aurum Tech LLC is burgeoning tech company focused on blockchain 
-            and web development. We build interesting and secure dApps, webapps,
-            and a myriad of other projects related to technology. Aurum Technologies
-            is a small-sized company with big goals. We take donations if you enjoy 
-            using our products and we're open to collaboration with interested patrons.
-            Our currently operating applications include: Blokracy (A dApp utilizing the 
-            blockchain to support a public voting ecosystem.) and Persona256 (A dApp 
-            used for decentralized identification, meant to be used for the secure 
-            transfer of data). With many more projects to come, feel to join our  
-            newsletter and stay up to date.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vel velit elit. Ut dignissim pulvinar ante,
+                at laoreet justo feugiat non. Aenean quam diam, posuere at massa eget, molestie congue massa. Donec sed
+                consequat turpis. Cras ut dolor lacus. Maecenas euismod ex id tortor pellentesque tincidunt. Morbi
+                vehicula vulputate nibh. Suspendisse non tristique ex, in euismod dolor. Duis vulputate orci risus, sit 
+                amet malesuada ex aliquam et. Nam vulputate lacus a nulla dapibus pharetra. Etiam mollis ligula nec semper 
+                gravida. Maecenas est risus, varius in mauris in, elementum rutrum velit. Mauris dapibus ut mi ut 
+                ultricies. Nullam vel porttitor lacus.
+
+                Fusce vel augue ac ipsum semper mollis vitae et eros. Praesent metus libero, fermentum non leo consectetur, 
+                pharetra ullamcorper nunc. Donec dictum condimentum pulvinar. Praesent fringilla in magna ut consequat. 
+                Proin et tellus ut dui laoreet sollicitudin vel vitae orci. Phasellus non dolor posuere, maximus est quis, 
+                varius ipsum. Aliquam id mi faucibus, aliquam leo ut, fermentum velit. Donec sit amet commodo odio.
             </i></p>
             <br /><br />
         </div>
